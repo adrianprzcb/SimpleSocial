32065c4a641f6a0004b749fe554bae153090c8c0
adrianprzcb <adrianpc4444@gmail.com>
2024-11-12 11:26:34 +0100

    Initial commit

---
+++ src/main/java/com/adrian/SimpleSocial/SimpleSocialApplication.java
@@ -0,0 +1,13 @@
+package com.adrian.SimpleSocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSocialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adrian/SimpleSocial/SimpleSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrian.SimpleSocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
